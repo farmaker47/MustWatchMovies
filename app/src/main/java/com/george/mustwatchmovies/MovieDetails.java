8d6891ba71208d433f9761903582436d447af3a9
farmaker47 <farmaker47@yahoo.cm>
2018-02-24 11:30:26 +0200

    Videos and Share ready

--- app/src/main/java/com/george/mustwatchmovies/MovieDetails.java
+++ app/src/main/java/com/george/mustwatchmovies/MovieDetails.java
@@ -115,7 +115,6 @@ public class MovieDetails extends AppCompatActivity implements LoaderManager.Loa
 
                 case R.drawable.heart:
                     fab.setImageResource(R.drawable.heart_out);
-                    fab.setTag(0);
                     fab.setTag(R.drawable.heart_out);
                     //method to erase movie from favorites
                     deleteInfoFromDB();
